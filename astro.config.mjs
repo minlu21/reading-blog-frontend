b5d39b4273252fd5f799ad8743dba89f0bcaed5c
Min Lu <luming2k10@gmail.com>
2025-07-26 23:02:17 -0400

    publish v1 of reading blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity({
+    projectId: 'sss90d9t',
+    dataset: 'production',
+    useCdn: false, // See note on using the CDN
+    apiVersion: "2025-07-01"
+  }), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/components/AstroTabs.tsx
@@ -0,0 +1,71 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function AstroTabs({ title, tabNames, tabContent }) {
+  function extractBooks(tabContent) {
+    if (tabContent) {
+      return (
+        <ul className="grid grid-cols-2 gap-6">
+          {tabContent.map((tab, idx) => (
+            <li key={idx}>
+              <a
+                href={"/books/" + tab.slug.current}
+                className={
+                  tab.status === "Completed"
+                    ? "text-green-700 line-through"
+                    : tab.status === "In Progress"
+                    ? "text-slate-900 underline"
+                    : tab.status === "Abandoned"
+                    ? "text-red-700 line-through"
+                    : "text-slate-600"
+                }
+              >
+                {tab.name}
+              </a>{" "}
+              <span>
+                ({tab.publishYear}
+                {tab.bc ? " B.C." : ""})
+              </span>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <Tabs defaultValue={title} className="m:min-w-2xl lg:min-w-3xl">
+      <TabsList>
+        {tabNames.map((tab, idx) => (
+          <TabsTrigger key={idx} value={tab}>
+            {tab}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+
+      <TabsContent value={tabNames[0]}>
+        {/* <ExampleCombobox client:load /> */}
+        {extractBooks(tabContent[0])}
+      </TabsContent>
+
+      <TabsContent value={tabNames[1]}>
+        {/* <ExampleCombobox client:load /> */}
+        {extractBooks(tabContent[1])}
+      </TabsContent>
+
+      <TabsContent value={tabNames[2]}>
+        {/* <ExampleCombobox client:load /> */}
+        {extractBooks(tabContent[2])}
+      </TabsContent>
+
+      <TabsContent value={tabNames[3]}>
+        {/* <ExampleCombobox client:load /> */}
+        {extractBooks(tabContent[3])}
+      </TabsContent>
+
+      <TabsContent value={tabNames[4]}>
+        {/* <ExampleCombobox client:load /> */}
+        {extractBooks(tabContent[4])}
+      </TabsContent>
+    </Tabs>
+  );
+}
