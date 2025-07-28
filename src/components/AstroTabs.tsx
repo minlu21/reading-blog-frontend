ce6cc799111422dcefc8a2c3a7eb2ad80cf88d23
Min Lu <luming2k10@gmail.com>
2025-07-28 10:59:09 -0400

    added status identifier on book list

--- src/components/AstroTabs.tsx
+++ src/components/AstroTabs.tsx
@@ -4,7 +4,7 @@ export default function AstroTabs({ title, tabNames, tabContent }) {
   function extractBooks(tabContent) {
     if (tabContent) {
       return (
-        <ul className="grid grid-cols-2 gap-6">
+        <ul id="bookList" className="grid grid-cols-2 gap-6">
           {tabContent.map((tab, idx) => (
             <li key={idx}>
               <a
@@ -13,7 +13,7 @@ export default function AstroTabs({ title, tabNames, tabContent }) {
                   tab.status === "Completed"
                     ? "text-green-700 line-through"
                     : tab.status === "In Progress"
-                    ? "text-slate-900 underline"
+                    ? "text-slate-900 underline font-bold"
                     : tab.status === "Abandoned"
                     ? "text-red-700 line-through"
                     : "text-slate-600"
